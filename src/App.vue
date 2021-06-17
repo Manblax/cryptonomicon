a8fd0c35921a257ac27021bef916571597d089bd
manblax <manblax@yandex.ru>
2021-06-17 19:20:03 +0300

    Перенести код добавления тикера в AddTicker.vue

--- src/App.vue
+++ src/App.vue
@@ -11,58 +11,7 @@
 
     <div class="container">
       <div class="w-full my-4"></div>
-      <section>
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700"
-            >Тикер</label
-            >
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                  v-model="search"
-                  @keydown.enter="addTicker"
-                  type="text"
-                  name="wallet"
-                  id="wallet"
-                  autocomplete="off"
-                  class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                  placeholder="Например DOGE"
-              />
-            </div>
-            <div v-if="makeSuggestions().length" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-              <span
-                  v-for="coinSuggest of makeSuggestions()"
-                  @click="addSuggest(coinSuggest)"
-                  :key="coinSuggest.Id"
-                  class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                {{ coinSuggest.Symbol }}
-              </span>
-            </div>
-            <div v-if="tickerError" class="text-sm text-red-600">Такой тикер уже добавлен</div>
-          </div>
-        </div>
-        <button
-            @click="addTicker"
-            type="button"
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          <!-- Heroicon name: solid/mail -->
-          <svg
-              class="-ml-0.5 mr-2 h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 24 24"
-              fill="#ffffff"
-          >
-            <path
-                d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Добавить
-        </button>
-      </section>
+      <AddTicker :tickers="tickers" @add-ticker="addTicker"></AddTicker>
 
       <div class="flex items-end mt-4">
         <div class="w-1/5">
@@ -188,16 +137,18 @@
 
 <script>
 import {fetchCoinList, subscribeToTicker} from '@/api';
+import AddTicker from "./components/AddTicker";
 
 export default {
   name: 'App',
+  components: {
+    AddTicker
+  },
   data() {
     return {
-      search: '',
       tickers: [],
       selectedTicker: null,
       graph: [],
-      tickerError: false,
       filter: '',
       page: 1,
       graphMaxSize: 1,
@@ -259,39 +210,16 @@ export default {
       console.log('this.max', this.graphMaxSize);
     },
 
-    addSuggest(coinSuggest) {
-      //console.log('coinSuggest', coinSuggest)
-      this.search = coinSuggest.Symbol;
-      this.addTicker();
-    },
-    makeSuggestions() {
-      if (!this.search.trim()) return [];
 
-      let coinList = JSON.parse(sessionStorage.getItem('coinlist')) || [];
-      coinList = Object.values(coinList.Data);
-      coinList = coinList.filter(coin => coin.FullName.toLowerCase().includes(this.search.trim().toLowerCase()));
-      coinList.splice(4);
 
-      return coinList;
-    },
     deleteTicker(tickerToRemove) {
       this.tickers = this.tickers.filter(ticker => ticker !== tickerToRemove);
       if (this.selectedTicker === tickerToRemove) {
         this.selectedTicker = null;
       }
     },
-    async addTicker() {
-      if (!this.search) return;
-
-      const isIncludes = this.tickers.some(ticker => ticker.title.toLowerCase() === this.search.toLowerCase());
-      if (isIncludes) return this.tickerError = true;
-
-      const newTicker = {
-        title: this.search.toUpperCase(),
-        price: '-'
-      };
+    async addTicker(newTicker) {
       this.tickers = [...this.tickers, newTicker];
-      this.search = '';
       this.filter = '';
       subscribeToTicker(newTicker.title, (newPrice) => {
         this.updateTicker(newTicker.title, newPrice);
@@ -360,12 +288,7 @@ export default {
         this.calcGraphMaxSize();
       });
     },
-    search() {
-      if (this.tickerError) {
-        this.tickerError = false;
-      }
-      this.makeSuggestions();
-    },
+
     tickers() {
       localStorage.setItem('tickerList', JSON.stringify(this.tickers));
     }
---
+++ src/components/AddTicker.vue
@@ -0,0 +1,115 @@
+<template>
+  <section>
+    <div class="flex">
+      <div class="max-w-xs">
+        <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
+        <div class="mt-1 relative rounded-md shadow-md">
+          <input
+              v-model="search"
+              @keydown.enter="addTicker"
+              type="text"
+              name="wallet"
+              id="wallet"
+              autocomplete="off"
+              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+              placeholder="Например DOGE"
+          />
+        </div>
+        <div v-if="makeSuggestions.length" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+              <span
+                  v-for="coinSuggest of makeSuggestions"
+                  @click="addSuggest(coinSuggest)"
+                  :key="coinSuggest.Id"
+                  class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+              >
+                {{ coinSuggest.Symbol }}
+              </span>
+        </div>
+        <div v-if="tickerError" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+      </div>
+    </div>
+    <button
+        @click="addTicker"
+        :disabled="hasTicker"
+        type="button"
+        class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    >
+      <svg
+          class="-ml-0.5 mr-2 h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="#ffffff"
+      >
+        <path
+            d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+        ></path>
+      </svg>
+      Добавить
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AddTicker",
+  props: {
+    tickers: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      search: '',
+      tickerError: false,
+    }
+  },
+  emits: ['add-ticker'],
+  methods: {
+    addTicker() {
+      if (!this.search || this.hasTicker) return;
+
+      const newTicker = {
+        title: this.search.toUpperCase(),
+        price: '-'
+      };
+
+      this.search = '';
+      this.$emit('add-ticker', newTicker);
+    },
+
+    addSuggest(coinSuggest) {
+      this.search = coinSuggest.Symbol;
+      this.addTicker();
+    },
+  },
+  computed: {
+    makeSuggestions() {
+      if (!this.search.trim()) return [];
+
+      let coinList = JSON.parse(sessionStorage.getItem('coinlist')) || [];
+      coinList = Object.values(coinList.Data);
+      coinList = coinList.filter(coin => coin.FullName.toLowerCase().includes(this.search.trim().toLowerCase()));
+      coinList.splice(4);
+
+      return coinList;
+    },
+    hasTicker() {
+      return this.tickers.some(ticker => ticker.title.toLowerCase() === this.search.toLowerCase());
+    },
+  },
+  watch: {
+    search() {
+      this.tickerError = this.hasTicker;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
